db74ff6862dc2ef80eb31d14779b7355df492d44
Madiyar <nu_ofsn11@mail.ru>
2020-02-06 14:54:35 +0600

    First files

---
+++ oHappy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace oHappy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] array = { "The capital of the Great Britain","What is the square root of 256",
+                "Н2О. This is the formula of what","How many planets in the solar system", "2018 World Cup Champion" };
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
