9770469abafd0df438d7050292881724caa59ff7
Madiyar <nu_ofsn11@mail.ru>
2020-02-10 11:53:57 +0600

    questions and right answers through multy arrays

--- oHappy/Program.cs
+++ oHappy/Program.cs
@@ -7,39 +7,44 @@ namespace oHappy
     {
         static void Main(string[] args)
         {
-
+            //used variables
             string[] questions = { "The capital of the Great Britain","What is the square root of 256",
                 "Н2О. This is the formula of what","How many planets in the solar system", "2018 World Cup Champion" };
             string[] variance = { "a", "b", "c", "d" };
-            string[] rightAnswers = { "London", "16", "water", "8", "France"};
-            string[,] answers = new string[5, 4] { { "London","Paris","Washington","Edinburgh"},
-                {"16","25","9","2" },{ "water", "salt", "gas", "spirt" },{ "9", "8", "16", "2000" },
-                { "France", "Spain", "Croatia", "Argentina" } };
-           
-            Dictionary<string,string> quan = new Dictionary<string,string>();
-           
+            string[] rightAnswers = new string[5];
+            string[][] answers = new string[5][];
+            answers[0] = new string[] { "London", "Paris", "Washington", "Edinburgh" };
+            answers[1] = new string[] { "16", "25", "9", "2" };
+            answers[2] = new string[] { "water", "salt", "gas", "alcohol" };
+            answers[3] = new string[] { "8", "9", "16", "2000" };
+            answers[4] = new string[] { "France", "Spain", "Croatia", "Argentina" };
+            string userAnswer;
+            Dictionary<string, string> quan = new Dictionary<string, string>();
 
+
+            
             for (int i = 0; i < questions.Length; i++)
-            { 
-                //Console.WriteLine(questions[i]);
-                quan.Add(questions[i],rightAnswers[i]);
+            {
+                quan.Add(questions[i], answers[i][0]);
+                rightAnswers[i] = answers[i][0];
+                Array.Sort(answers[i]);
             }
 
-            string userAnswer;
-            for(int i = 0; i < questions.Length; i++)
+            //game start
+            for (int i = 0; i < questions.Length; i++)
             {
                 bool isValid = false;
                 while (!isValid)
                 {
-                    
+
                     Console.WriteLine($"{i + 1}) " + questions[i] + "?");
                     for (int j = 0; j < 4; j++)
                     {
-                        Console.Write(variance[j].ToUpper()+") "+answers[i,j]+" ");
+                        Console.Write(variance[j].ToUpper() + ") " + answers[i][j] + " ");
                     }
                     Console.WriteLine();
                     userAnswer = Console.ReadLine();
-                    foreach(string el in variance)
+                    foreach (string el in variance)
                     {
                         if (userAnswer == el)
                         {
@@ -48,17 +53,33 @@ namespace oHappy
                         }
                         else if (userAnswer == "q") return;
                     }
-                    
+
                 }
-                
+
 
             }
 
-
-
-
             Console.WriteLine();
             Console.ReadLine();
+        }
+
+        public static void ShowBoard(string[] arr){
+            foreach(string el in arr)
+            {
+                Console.Write(el+", ");
+            }
+        }
+
+        public static void ShowTwoArr(string[][] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Console.Write(arr[i][j] + ", ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
